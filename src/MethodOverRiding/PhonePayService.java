d18889c436d462c9be5ac16b1a351a7c70c8acd7
chandrakala <chandraklag688@gmail.com>
2024-02-20 10:52:25 +0530

    MethodOverRiding

---
+++ src/MethodOverRiding/PhonePayService.java
@@ -0,0 +1,10 @@
+package MethodOverRiding;
+
+public class PhonePayService extends BankAccountService{
+    public boolean transfer(String fromMobileNumber,String toMobileNumber,int amount){
+        System.out.println("from subClass BankAccount");
+        return super.transfer(fromMobileNumber,toMobileNumber,amount);
+
+    }
+
+}
